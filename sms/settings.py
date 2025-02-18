203e299b60fc9c6d777c3a4dd13399837bca1b97
annmwangi04 <annmwas204@gmail.com>
2025-02-18 14:09:29 +0300

    updating my db

--- sms/settings.py
+++ sms/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 
 # 
 
-DATABASES={
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+DATABASES = {
+    "default": dj_database_url.parse(config("DATABASE_URL"))
 }
 #Cloudinary configuration
 cloudinary.config (
